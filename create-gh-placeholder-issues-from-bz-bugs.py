21883b81a8429598bd429c84259e949dca676a3e
Konrad Kleine <kkleine@redhat.com>
2020-04-29 08:48:02 +0200

    Improve time for checking already up to date github issues by fetching bugzillas in batches of 100

--- create-gh-placeholder-issues-from-bz-bugs.py
+++ create-gh-placeholder-issues-from-bz-bugs.py
@@ -7,8 +7,6 @@ import sys
 import time
 import logging
 
-
-
 # Setup logging to file and to stdout/stderr
 logger = logging.getLogger("bz2gh")
 logger.setLevel(logging.DEBUG)
@@ -37,37 +35,39 @@ repo = gh.get_repo(config.GH_REPO)
 
 # Get remaining github requests once and later check and update it but work
 # with estimated numbers from here on.
-remaining_requests = 0
-remaining_requests = gh.get_rate_limit().core.remaining
-remaining_requests_last_refreshed = time.time()
-logger.debug("Remaining github requests: %d", remaining_requests)
+remaining_github_api_requests = 0
+remaining_github_api_requests = gh.get_rate_limit().core.remaining
+remaining_github_api_requests_last_refreshed = time.time()
+logger.debug("Remaining github API requests: %d", remaining_github_api_requests)
 
 def ensure_enough_requests(ask_for_num_requests=1):
-    global remaining_requests
-    global remaining_requests_last_refreshed
-    if remaining_requests <= ask_for_num_requests or (time.time() - remaining_requests_last_refreshed) > 60:
-        while True:
-            remaining_requests = gh.get_rate_limit().core.remaining
-            remaining_requests_last_refreshed = time.time()
-            logger.debug("Refreshed remaining github request: %d remaining", remaining_requests)
-            if remaining_requests <= ask_for_num_requests:
-                seconds_to_wait = 300
-                logger.warning("Number of remaining Github requests is too low (%d). Waiting %fs until we can continue." % (remaining_requests, seconds_to_wait))
-                time.sleep(seconds_to_wait)
-            else:
-                return
+    global remaining_github_api_requests
+    global remaining_github_api_requests_last_refreshed
+    if remaining_github_api_requests > ask_for_num_requests or (time.time() - remaining_github_api_requests_last_refreshed) < 60:
+        return
+    
+    while True:
+        remaining_github_api_requests = gh.get_rate_limit().core.remaining
+        remaining_github_api_requests_last_refreshed = time.time()
+        logger.debug("Refreshed remaining github API request: %d remaining", remaining_github_api_requests)
+        if remaining_github_api_requests <= ask_for_num_requests:
+            seconds_to_wait = 300
+            logger.warning("Number of remaining github API requests is too low (%d). Waiting %fs until we can continue." % (remaining_github_api_requests, seconds_to_wait))
+            time.sleep(seconds_to_wait)
+        else:
+            return
 
 def retry_github_action(func, type, max_retries=10, **kwargs):
-    global remaining_requests
+    global remaining_github_api_requests
     i = 0
     while i < max_retries:
         i += 1
         try:
             ensure_enough_requests(1)
             res = func(**kwargs)
-            remaining_requests -= 1
+            remaining_github_api_requests -= 1
             return res
-        except github.GithubException:
+        except (github.GithubException, github.UnknownObjectException):
             logger.warning("Retrying github '%s' call for at most %d more time(s)", type, max_tries - i)
             max_retries -= 1
             time.sleep(0.8*(i+1))
@@ -75,96 +75,112 @@ def retry_github_action(func, type, max_retries=10, **kwargs):
     logger.error("Failed to retry ")
     raise Exception("")
 
-def main():
+def import_bz(bug):
     global bzapi
     global gh
     global logger
     global repo
+    global remaining_github_api_requests
 
-    issue_id = 0
-    # optionally start at a given id
-    if len(sys.argv) == 2:
-        issue_id = int(sys.argv[1]) - 1
+    issue_id = bug.id
 
-    while True:
-        issue_id += 1
+    # Decide if issue will be created or updated
+    create_or_update = "create"
+    issue = None
+    ensure_enough_requests(1)
+    try:
+        issue = repo.get_issue(issue_id)
+        create_or_update = "update"
+        remaining_github_api_requests -= 1
+    except github.UnknownObjectException:
+        pass
 
-        # Make sure the bug exists in bugzilla (if not, we'll stop right here)
-        bug = None
-        try:
-            bug = bzapi.getbug(issue_id)
-        except Exception as e:
-            print("failed to query for bugzilla %d: %s" % (issue_id, e))
-            break
+    github_issue_url = "https://github.com/%s/issues/%d" % (config.GH_REPO, issue_id)
+    bugzilla_bug_url = "%s/show_bug.cgi?id=%d" % (config.BZURL, issue_id)
 
-        # Decide if issue will be created or updated
-        create_or_update = "create"
-        issue = None
-        ensure_enough_requests()
-        try:
-            issue = repo.get_issue(issue_id)
-            create_or_update = "update"
-        except github.UnknownObjectException:
-            pass
+    # Prepare values for github issue
+    labels = [bug.product + "/" + bug.component,
+            "dummy import from bugzilla",
+            "BZ-BUG-STATUS: %s" % bug.bug_status]
+    if bug.resolution != "":
+        labels.append("BZ-RESOLUTION: %s" % bug.resolution)
 
-        # Prepare values for github issue
-        imported_from_url = "%s/show_bug.cgi?id=%d" % (config.BZURL, issue_id)
-        labels = [bug.product + "/" + bug.component,
-                "dummy import from bugzilla",
-                "BZ-BUG-STATUS: %s" % bug.bug_status]
-        if bug.resolution != "":
-            labels.append("BZ-RESOLUTION: %s" % bug.resolution)
+    body = "This issue was imported from Bugzilla %s." % bugzilla_bug_url
+    title = bug.short_desc
+    # Unfortunately github requires to specify a lock reason from a fixed list.
+    # https://developer.github.com/v3/issues/#parameters-6
+    lock_reason = "too heated"
+    # logic to decide if an issue is supposed to be closed or kept open.
+    state = "open"
+    if bug.bug_status == "RESOLVED" or bug.status == "CLOSED" or bug.status == "VERIFIED":
+        if bug.resolution == "FIXED" or bug.resolution == "INVALID" or bug.resolution == "WONTFIX" or bug.resolution == "DUPLICATE" or bug.resolution == "WORKSFORME":
+            state = "closed"
 
-        body = "This issue was imported from Bugzilla %s." % imported_from_url
-        title = bug.short_desc
-        # Unfortunately github requires to specify a lock reason from a fixed list.
-        # https://developer.github.com/v3/issues/#parameters-6
-        lock_reason = "too heated"
-        # logic to decide if an issue is supposed to be closed or kept open.
-        state = "open"
-        if bug.bug_status == "RESOLVED" or bug.status == "CLOSED" or bug.status == "VERIFIED":
-            if bug.resolution == "FIXED" or bug.resolution == "INVALID" or bug.resolution == "WONTFIX" or bug.resolution == "DUPLICATE" or bug.resolution == "WORKSFORME":
-                state = "closed"
-
-        if issue == None:
-            logger.info("Creating github issue https://github.com/%s/issues/%d from BZ %s" % (config.GH_REPO, issue_id, imported_from_url))
-            retry_github_action(repo.create_issue, type="create issue", title=title, labels=labels, body=body)
+    if issue == None:
+        logger.info("Creating github issue %s from BZ %s" % (github_issue_url, bugzilla_bug_url))
+        retry_github_action(repo.create_issue, type="create issue", title=title, labels=labels, body=body)
+    else:
+        current_labels = []
+        for l in issue.labels:
+            current_labels.append(l.name)
+        if title != issue.title or body != issue.body or set(labels) != set(current_labels):
+            logger.info("Updating github issue %s from BZ %s" % (github_issue_url, bugzilla_bug_url))
+            retry_github_action(issue.edit, type="update issue", title=title, body=body, labels=labels)
         else:
-            current_labels = []
-            for l in issue.labels:
-                current_labels.append(l.name)
-            if title != issue.title or body != issue.body or set(labels) != set(current_labels):
-                logger.info("Updating github issue https://github.com/%s/issues/%d from BZ %s" % (config.GH_REPO, issue_id, imported_from_url))
-                retry_github_action(issue.edit, type="update issue", title=title, body=body, labels=labels)
-            else:
-                logger.info("Github issue https://github.com/%s/issues/%d already up to date with BZ from %s" % (config.GH_REPO, issue_id, imported_from_url))
+            logger.info("Github issue %s already up to date with BZ from %s" % (github_issue_url, bugzilla_bug_url))
+    
+    current_state = "open"
+    if create_or_update == "update":
+        current_state = issue.state
+    
+    # If the issue is new, we need to lock it later and here we're fetching the
+    # issue repeatidly from github after we've just created it.
+    if create_or_update == "create":
+        issue = retry_github_action(repo.get_issue, type="get issue", number=issue_id)
 
-        current_state = "open"
-        if create_or_update == "update":
-            current_state = issue.state
-
-        # If the issue is new, we need to lock it later and here we're fetching the
-        # issue repeatidly from github after we've just created it.
-        if create_or_update == "create":
-            issue = retry_github_action(repo.get_issue, type="get issue", number=issue_id)
-
-        # Add a state change comment if the previous state was open and now is 
-        # closed or if it was closed and now is open.
-        if state != current_state:
-            state_change_comment = "issue because of bugzilla's bug state (%s) and resolution (%s)." % (bug.bug_status, bug.resolution)
-            if state == "closed":
-                state_change_comment = "Closing " + state_change_comment
-            if state == "open":
-                state_change_comment = "Re-opening " + state_change_comment
-            retry_github_action(issue.create_comment, type="create state change comment", body=state_change_comment)
-            retry_github_action(issue.edit, type="change issue state", state=state)
-
-        # Now lock the issue to prevent anything happening on this issue.
-        if create_or_update == "create":
+    # Add a state change comment if the previous state was open and now is 
+    # closed or if it was closed and now is open.
+    if state != current_state:
+        state_change_comment = "issue because of bugzilla's bug state (%s) and resolution (%s)." % (bug.bug_status, bug.resolution)
+        if state == "closed":
+            state_change_comment = "Closing " + state_change_comment
+        if state == "open":
+            state_change_comment = "Re-opening " + state_change_comment
+        retry_github_action(issue.create_comment, type="create state change comment", body=state_change_comment)
+        retry_github_action(issue.edit, type="change issue state", state=state)
+    
+    # Now lock the issue to prevent anything happening on this issue.
+    if create_or_update == "create":
+        retry_github_action(issue.lock, type="lock issue", lock_reason=lock_reason)
+    else:
+        if not issue.locked:
             retry_github_action(issue.lock, type="lock issue", lock_reason=lock_reason)
-        else:
-            if not issue.locked:
-                retry_github_action(issue.lock, type="lock issue", lock_reason=lock_reason)
 
 if __name__ == "__main__":
-    main()
+    # optionally start at a given id
+    start_with = 0
+    if len(sys.argv) == 2:
+        start_with = int(sys.argv[1])
+
+    # optionally fetch BZs in batches
+    batch_size = 100
+    if len(sys.argv) == 3:
+        batch_size = int(sys.argv[2])
+    logger.info("Fetching bugzilla bugs in batches of %d bugs", batch_size)
+
+    while True:
+        bz_batch = []
+        try:
+            bz_batch = bzapi.getbugs(range(start_with, start_with + batch_size), extra_fields=["labels", "short_desc", "bug_status", "resolution", "product", "component"])
+        except Exception as e:
+            logger.error("Failed to query for bugzillas %d to %d: %s", start_with, start_with + batch_size, e)
+            break
+
+        for bz in bz_batch:
+            if bz == None:
+                logger.info("No more bugzillas to process")
+            else:
+                # logger.info("Processing Bugzilla %d - %s", bz.id, bz.short_desc)
+                import_bz(bz)
+        
+        start_with += batch_size
